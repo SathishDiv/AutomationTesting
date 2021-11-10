70acef075a613ec181d582af2b7d6ca25725af0c
sathishsuriya <sathishsuriya@gmail.com>
2021-11-10 18:52:42 +0530

    first version

---
+++ CIProjectFour/src/CISampleScript.java
@@ -0,0 +1,16 @@
+
+public class CISampleScript {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+System.out.println("selenium");
+
+System.out.println("java");
+
+System.out.println("uft");
+
+System.out.println("oracle");
+	}
+
+}
